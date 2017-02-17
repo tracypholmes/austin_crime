2e66634dbe0dcf37ac26b9daec0f937f7371d03f
Tracy Holmes <tracypholmes@gmail.com>
2017-02-17 16:29:59 -0600

    updated bin with require; commented out test interface in crime.rb

--- lib/austin_crime/crime.rb
+++ lib/austin_crime/crime.rb
@@ -2,13 +2,13 @@ class AustinCrime::Crime
   attr_accessor :type, :date, :increp, :url
   def self.all
     # I should return a bunch of instances of Crime
-    puts <<-DOC
-    1. Assault data.
-    2. Burglary data.
-    3. Crash data.
-    4. DWI data.
-    5. Indecent Exposure data.
-    DOC
+    # puts <<-DOC
+    # 1. Assault data.
+    # 2. Burglary data.
+    # 3. Crash data.
+    # 4. DWI data.
+    # 5. Indecent Exposure data.
+    # DOC
     crime_1 = self.new
     crime_1.type = "THEFT"
     crime_1.date = "2016-02-06"
