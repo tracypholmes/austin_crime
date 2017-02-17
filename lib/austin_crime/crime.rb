2e5aae8e6ac9a370fe4e20acfc46a2801eba65aa
Tracy Holmes <tracypholmes@gmail.com>
2017-02-17 17:53:13 -0600

    crime.rb with method to pull data. updated to include HTTParty & pry

--- lib/austin_crime/crime.rb
+++ lib/austin_crime/crime.rb
@@ -1,22 +1,14 @@
 class AustinCrime::Crime
-  attr_accessor :type, :date, :increp, :url
+  attr_accessor :type, :type_hash, :date, :increp, :url
   def self.all
-    # I should return a bunch of instances of Crime
-    # puts <<-DOC
-    # 1. Assault data.
-    # 2. Burglary data.
-    # 3. Crash data.
-    # 4. DWI data.
-    # 5. Indecent Exposure data.
-    # DOC
-    crime_1 = self.new
-    crime_1.type = "THEFT"
-    crime_1.date = "2016-02-06"
-    crime_1.increp = "20165005327"
-    crime_1.url = "https://data.austintexas.gov/resource/rkrg-9tez.json"
+    # scrape opendata - use API to return crime based on that data
+    self.url
+  end
 
-    [crime_1]
-
+  def self.url(type)
+    url = "https://data.austintexas.gov/resource/rkrg-9tez.json?$where=starts_with(crime_type,'#{type}')"
+    response = HTTParty.get(url)
+    @type_hash = response.parsed_response
   end
 
 
