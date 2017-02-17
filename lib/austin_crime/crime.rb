de4a9cd5fe25167f9c948582ad1d74e5ca572192
Tracy Holmes <tracypholmes@gmail.com>
2017-02-17 15:52:07 -0600

    updated crime.rb wtih attr_accessors

--- lib/austin_crime/crime.rb
+++ lib/austin_crime/crime.rb
@@ -1,5 +1,5 @@
 class AustinCrime::Crime
-
+  attr_accessor :type, :date, :increp, :url
   def self.all
     # I should return a bunch of instances of Crime
     puts <<-DOC
@@ -9,6 +9,14 @@ class AustinCrime::Crime
     4. DWI data.
     5. Indecent Exposure data.
     DOC
+    crime_1 = self.new
+    crime_1.type = "THEFT"
+    crime_1.date = "2016-02-06"
+    crime_1.increp = "20165005327"
+    crime_1.url = "https://data.austintexas.gov/resource/rkrg-9tez.json"
+
+    [crime_1]
+
   end
 
 
