55800793f84e3af9300266345492b1fa13812a77
Tracy Holmes <tracypholmes@gmail.com>
2017-02-17 19:45:45 -0600

    Streamlined CLI inputs

--- lib/austin_crime/cli.rb
+++ lib/austin_crime/cli.rb
@@ -7,20 +7,28 @@ class AustinCrime::CLI
     puts "Welcome to Austin Crime Search!"
     inputs
     menu
-    cya
+  #  cya
   end
 
     def inputs
-      types = ["ASSAULT", "BURGLARY", "CRASH", "THEFT", "DRIVING"]
+      types = ["DWI", "HARASSMENT", "CRUELTY TO ANIMALS", "THEFT", "CRIMINAL MISCHIEF"]
 
  # Select type of crime to view
       puts <<-DOC
-      1. ASSAULT
-      2. BURGLARY
-      3. CRASH
+      1. DWI
+      2. HARASSMENT
+      3. CRUELTY TO ANIMALS
       4. THEFT
-      5. DRIVING
+      5. CRIMINAL MISCHIEF
       DOC
+
+      puts "\nPlease enter the number of the crime type for more information."
+      # or enter 'list' to see your list of options. "
+      puts "You may also enter 'q' to Quit."
+      input = gets.strip.downcase
+      input = input.to_i - 1
+    end
+      type = inputs[input]
 
       # @crimes = AustinCrime::Crime.all
       # @crimes.each.with_index(1) do |crime, i|
@@ -32,11 +40,6 @@ class AustinCrime::CLI
     def menu
       input = nil
       while input != "q"
-        puts "\nPlease enter the number of the crime type for more information."
-        # or enter 'list' to see your list of options. "
-        puts "You may also enter 'q' to Quit."
-        input = gets.strip.downcase
-        input = input.to_i - 1
 
       if input.to_i > 0 && input.to_i < 6
       #  puts "#{the_crime.type} - #{the_crime.increp} - #{the_crime.date}"
@@ -47,7 +50,6 @@ class AustinCrime::CLI
       else
         puts "Need some help? Type 'list' to see your options."
       end
-    #  type = inputs[input]
     end
   end
 
