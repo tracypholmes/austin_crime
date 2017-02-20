5b021c610086f93f48833d8aafd08d9980da3ff1
Tracy Holmes <tracypholmes@gmail.com>
2017-02-20 09:12:06 -0600

    changed 'dumped' to 'showing'; added horizontal line between indiv results

--- lib/austin_crime/cli.rb
+++ lib/austin_crime/cli.rb
@@ -1,54 +1,50 @@
 require 'httparty'
 
 class AustinCrime::CLI
-
   def initialize; end
 
   def call
-    puts "Welcome to Austin Crime Search!"
+    puts 'Welcome to Austin Crime Search!'
     inputs
     menu
-  #  cya
+    # cya
   end
 
-    def inputs
-  # ['DWI', 'HARASSMENT', 'CRUELTY TO ANIMALS', 'THEFT']
+  def inputs
+    # Select type of crime to view
+    puts <<-DOC
+    1. DWI
+    2. HARASSMENT
+    3. CRUELTY TO ANIMALS
+    4. THEFT
+    DOC
 
- # Select type of crime to view
-      puts <<-DOC
-      1. DWI
-      2. HARASSMENT
-      3. CRUELTY TO ANIMALS
-      4. THEFT
-      DOC
+    # @crimes = AustinCrime::Crime.all
+    # @crimes.each.with_index(1) do |crime, i|
+    #   puts "#{i}. #{crime.type} - #{crime.increp} - #{crime.date}"
+    # end
+  end
 
-
-      # @crimes = AustinCrime::Crime.all
-      # @crimes.each.with_index(1) do |crime, i|
-      #   puts "#{i}. #{crime.type} - #{crime.increp} - #{crime.date}"
-      # end
-    end
-
-    def menu
-      input = nil
-      while input != "q"
-        puts "\nPlease enter the number of the crime type for more information. Or enter 'list' to see your list of options."
-        puts "You may also enter 'q' to Quit."
-        input = gets.strip.downcase
-      # if input.to_i > 0 && input.to_i < 5
-      #   puts AustinCrime::Crime.all
+  def menu
+    input = nil
+    while input != 'q'
+      puts "\nPlease enter the number of the crime type for more information. Or enter 'list' to see your list of options."
+      puts "You may also enter 'q' to Quit."
+      input = gets.strip.downcase
+    # if input.to_i > 0 && input.to_i < 5
+      # puts AustinCrime::Crime.all
       case input
-      when "1"
+      when '1'
         puts AustinCrime::Crime.dwi
-      when "2"
+      when '2'
         puts AustinCrime::Crime.harassment
-      when "3"
+      when '3'
         puts AustinCrime::Crime.cruelty
-      when "4"
+      when '4'
         puts AustinCrime::Crime.theft
-      when "q"
-        puts "Thanks for visiting. Come again for more reasons NOT TO MOVE TO AUSTIN!"
-      when "list"
+      when 'q'
+        puts 'Thanks for visiting. Come again for more reasons NOT TO MOVE TO AUSTIN!'
+      when 'list'
         inputs
       else
         puts "Need some help? Type 'list' to see your options or 'q' to quit."
--- lib/austin_crime/crime.rb
+++ lib/austin_crime/crime.rb
@@ -2,32 +2,23 @@ require 'httparty'
 require 'pry'
 
 class AustinCrime::Crime
-  # attr_accessor :type, :type_hash, :date, :increp, :url
-  # scrape opendata - use API to return crime based on that data
-  #   self.url
-  # end
-  #
-  # def self.url
-  #   response = HTTParty.get('https://data.austintexas.gov/resource/rkrg-9tez.json')
-  #   puts response.body
-  # end
-
   def initialize; end
 
   def self.dwi
-    response = HTTParty.get("https://data.austintexas.gov/resource/rkrg-9tez.json?$where=starts_with(crime_type,'DWI')&$limit=5&$offset=5")
-    puts "Got #{response.count} results. Dumping first results:"
+    response = HTTParty.get("https://data.austintexas.gov/resource/rkrg-9tez.json?$where=starts_with(crime_type,'DWI')&$limit=10")
+    puts "Got #{response.count} results. Showing first result:"
     response.first.each do |k, v|
       puts "#{k}: #{v}"
     end
-    puts "enter 'more' if you'd like to browse more entries or 'menu' if you'd like to start over"
+    puts "\nEnter 'more' if you'd like to browse more entries or 'menu' if you'd like to start over"
     input = gets.chomp
     if input == 'more'
       response.collect do |hash|
+        puts '--------------------'
         hash.collect do |k, v|
-        puts "#{k}: #{v}"
+          puts "#{k}: #{v}"
+        end
       end
-    end
     elsif input == 'menu'
       AustinCrime::CLI.inputs
     else
@@ -37,67 +28,13 @@ class AustinCrime::Crime
 
   def self.harassment
     response = HTTParty.get("https://data.austintexas.gov/resource/rkrg-9tez.json?crime_type='HARASSMENT'")
-    puts "Got #{response.count} results. Dumping first results:"
-    response.first.each do |k, v|
-      puts "#{k}: #{v}"
-    end
-    puts "enter 'more' if you'd like to browse more entries or 'menu' if you'd like to start over"
-    input = gets.chomp
-    if input == 'more'
-      puts HTTParty.get("https://data.austintexas.gov/resource/rkrg-9tez.json?crime_type='HARASSMENT'")
-      response.each do |k, v|
-        puts "#{k}: #{v}"
-      end
-    elsif input == 'menu'
-      AustinCrime::CLI.inputs
-    else
-      puts 'Not sure what you want. Please try again.'
-    end
   end
 
   def self.cruelty
     response = HTTParty.get("https://data.austintexas.gov/resource/rkrg-9tez.json?crime_type='CRUELTY TO ANIMALS'")
-    puts "Got #{response.count} results. Dumping first results:"
-    response.first.each do |k, v|
-      puts "#{k}: #{v}"
-    end
-    puts "enter 'more' if you'd like to browse more entries or 'menu' if you'd like to start over"
-    input = gets.chomp
-    if input == 'more'
-      puts HTTParty.get("https://data.austintexas.gov/resource/rkrg-9tez.json?crime_type='CRUELTY TO ANIMALS'")
-      response.each do |k, v|
-        puts "#{k}: #{v}"
-      end
-    elsif input == 'menu'
-      AustinCrime::CLI.inputs
-    else
-      puts 'Not sure what you want. Please try again.'
-    end
   end
 
   def self.theft
     response = HTTParty.get("https://data.austintexas.gov/resource/rkrg-9tez.json?crime_type='THEFT'")
-    puts "Got #{response.count} results. Dumping first results:"
-    response.first.each do |k, v|
-      puts "#{k}: #{v}"
-    end
-    puts "enter 'more' if you'd like to browse more entries or 'menu' if you'd like to start over"
-    input = gets.chomp
-    if input == 'more'
-      puts HTTParty.get("https://data.austintexas.gov/resource/rkrg-9tez.json?crime_type='THEFT'")
-      response.each do |k, v|
-        puts "#{k}: #{v}"
-      end
-    elsif input == 'menu'
-      AustinCrime::CLI.inputs
-    else
-      puts 'Not sure what you want. Please try again.'
-    end
   end
-
-  # def self.incident
-  #   puts "\nPlease enter an Incident Report Number for more info"
-  #   number = ""
-  #   response =     HTTParty.get("https://data.austintexas.gov/resource/rkrg-9tez.json?incident_report_number='#{number}")
-  # end
 end
