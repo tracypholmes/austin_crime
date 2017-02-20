f1cca4bf17e4ae48f6550dbcf7f5be8a0c0952cb
Tracy Holmes <tracypholmes@gmail.com>
2017-02-20 12:07:08 -0600

    added method yeesh to return results

--- lib/austin_crime/crime.rb
+++ lib/austin_crime/crime.rb
@@ -2,28 +2,13 @@ require 'httparty'
 require 'pry'
 
 class AustinCrime::Crime
-  def initialize; end
+  def initialize
+    @response = response
+  end
 
   def self.dwi
-    response = HTTParty.get("https://data.austintexas.gov/resource/rkrg-9tez.json?$where=starts_with(crime_type,'DWI')&$limit=10")
-    puts "Got #{response.count} results. Showing first result:"
-    response.first.each do |k, v|
-      puts "#{k}: #{v}"
-    end
-    puts "\nEnter 'more' if you'd like to browse more entries or 'menu' if you'd like to start over"
-    input = gets.chomp
-    if input == 'more'
-      response.collect do |hash|
-        puts '--------------------'
-        hash.collect do |k, v|
-          puts "#{k}: #{v}"
-        end.compact.reject(&:empty?)
-      end
-    elsif input == 'menu'
-      AustinCrime::CLI.inputs
-    else
-      puts 'Not sure what you want. Please try again.'
-    end
+    @response = HTTParty.get("https://data.austintexas.gov/resource/rkrg-9tez.json?$where=starts_with(crime_type,'DWI')&$limit=10")
+    yeesh
   end
 
   def self.harassment
@@ -36,5 +21,26 @@ class AustinCrime::Crime
 
   def self.theft
     response = HTTParty.get("https://data.austintexas.gov/resource/rkrg-9tez.json?crime_type='THEFT'")
+  end
+
+  def self.yeesh
+    puts "Got #{@response.count} results. Showing first result:"
+    @response.first.each do |k, v|
+      puts "#{k}: #{v}"
+    end
+    puts "\nEnter 'more' if you'd like to browse more entries or 'menu' if you'd like to start over"
+    input = gets.chomp
+    if input == 'more'
+      @response.collect do |hash|
+        puts '--------------------'
+        hash.collect do |k, v|  # not supposed to use collect on hashes?
+          puts "#{k}: #{v}"
+        end.compact.reject(&:empty?) # c_cole on the assist
+      end
+    elsif input == 'menu'
+      AustinCrime::CLI.inputs
+    else
+      puts 'Not sure what you want. Please try again.'
+    end
   end
 end
